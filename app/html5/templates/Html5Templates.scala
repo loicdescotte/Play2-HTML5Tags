a35ef32edeafbf7600c13ee207ad0c694cadf2ac
Loic Descotte <loic.descotte@gmail.com>
2012-06-07 15:40:53 +0200

    manage maxlength with pattern

--- app/html5/templates/Html5Templates.scala
+++ app/html5/templates/Html5Templates.scala
@@ -12,8 +12,8 @@ object Html5Templates {
     field.constraints.foreach(
 	constraint => constraint._1 match {
         case "constraint.required" => out + Html(" required")
-        case "constraint.minLength" => out + constraintValue("minlength", constraint._2)
         case "constraint.maxLength" => out + constraintValue("maxlength", constraint._2)
+        case "constraint.minLength" => out + Html(" pattern=\".{" + constraint._2.head + ",}\"")
         // not working yet see issues
         //case "constraint.pattern" => out + constraintValue("pattern", constraint._2)
         case _ => ()
@@ -22,11 +22,11 @@ object Html5Templates {
   }
   
   private def constraintValue(label: String, values : Seq[Any]) = {
-	  val value = values.size match {
-      	case 0 => None
-      	case _ => Some(values.head)
-      }  
-	  val out = value.map(" " + label + "=\"" + _ + "\"")
-	  Html(out.getOrElse(""))
+	val value = values.size match {
+      	    case 0 => None
+      	    case _ => Some(values.head)
+        }  
+	val out = value.map(" " + label + "=\"" + _ + "\"")
+	Html(out.getOrElse(""))
   }
 }
