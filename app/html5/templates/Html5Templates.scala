fbaac43f15262452b7a31e1088131357036df211
loicdescotte <loicdescotte@gmail.com>
2012-07-04 18:05:39 +0200

    Float

--- app/html5/templates/Html5Templates.scala
+++ app/html5/templates/Html5Templates.scala
@@ -10,13 +10,13 @@ object Html5Templates {
    */
   def findType(value : Any, field: play.api.data.Field):Html = {
     field.constraints.foreach(
-    	constraint => if(constraint._1.equals("constraint.email"))
-		return Html.empty + Html("email")
+    	constraint => 
+	if(constraint._1.equals("constraint.email"))
+	  return Html.empty + Html("email")
     )
     val out = Html.empty
     value match {
-	//TODO other numeric types
-    	case _ : Int| _ : Long  => out + Html("number")
+    	case _ : Int| _ : Long  | _ : Float => out + Html("number")
     	case _ : Date  => out + Html("number")
     	case _ => out + Html("text")
     }	
@@ -29,23 +29,23 @@ object Html5Templates {
   def toValidation(field: play.api.data.Field) = {
     val out = Html.empty
     field.constraints.foreach(
-	constraint => constraint._1 match {
-        case "constraint.required" => out + Html(" required")
-        case "constraint.maxLength" => out + constraintValue("maxlength", constraint._2)
-        case "constraint.minLength" => out + Html(" pattern=\".{" + constraint._2.head + ",}\"")
-        // not working yet see issues
-        //case "constraint.pattern" => out + constraintValue("pattern", constraint._2)
-        case _ => ()
-      })
-    out
+      constraint => 
+      constraint._1 match {
+          case "constraint.required" => out + Html(" required")
+          case "constraint.maxLength" => out + constraintValue("maxlength", constraint._2)
+          case "constraint.minLength" => out + Html(" pattern=\".{" + constraint._2.head + ",}\"")
+          case "constraint.pattern" => out + constraintValue("pattern", constraint._2)
+          case _ => ()
+       })
+      out
   }
   
   private def constraintValue(label: String, values : Seq[Any]) = {
-	val value = values.size match {
-      	    case 0 => None
-      	    case _ => Some(values.head)
+    val value = values.size match {
+     	  case 0 => None
+      	  case _ => Some(values.head)
         }  
-	val out = value.map(" " + label + "=\"" + _ + "\"")
-	Html(out.getOrElse(""))
+    val out = value.map(" " + label + "=\"" + _ + "\"")
+    Html(out.getOrElse(""))
   }
 }
