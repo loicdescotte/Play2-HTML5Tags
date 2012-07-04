0ab739b1d408bcec81c03c6b396a880940ef70b8
Loic Descotte <loic.descotte@gmail.com>
2012-07-04 21:07:43 +0200

    detect emails

--- app/html5/templates/Html5Templates.scala
+++ app/html5/templates/Html5Templates.scala
@@ -5,22 +5,13 @@ import java.util._
 
 object Html5Templates {
 
-  /**
-   * Generates type attribute
-   */
-  def findType(value : Any, field: play.api.data.Field):Html = {
-    field.constraints.foreach(
-    	constraint => 
+  def findType(field: play.api.data.Field):Html = {
+   val out = Html.empty 
+   field.constraints.foreach(constraint => 
 	if(constraint._1.equals("constraint.email"))
-	  return Html.empty + Html("email")
+	  return Html("email")
     )
-    val out = Html.empty
-    value match {
-    	case _ : Int| _ : Long  | _ : Float => out + Html("number")
-    	case _ : Date  => out + Html("number")
-    	case _ => out + Html("text")
-    }	
-    out
+    Html("text")
   }
   
   /**
@@ -28,13 +19,12 @@ object Html5Templates {
    */
   def toValidation(field: play.api.data.Field) = {
     val out = Html.empty
-    field.constraints.foreach(
-      constraint => 
+    field.constraints.foreach(constraint => 
       constraint._1 match {
           case "constraint.required" => out + Html(" required")
           case "constraint.maxLength" => out + constraintValue("maxlength", constraint._2)
           case "constraint.minLength" => out + Html(" pattern=\".{" + constraint._2.head + ",}\"")
-          case "constraint.pattern" => out + constraintValue("pattern", constraint._2)
+          //case "constraint.pattern" => out + constraintValue("pattern", constraint._2)
           case _ => ()
        })
       out
