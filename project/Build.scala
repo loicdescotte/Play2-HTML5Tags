8e6d90f22dcdf5b8d1d735686b25bcdffb074e46
Loic Descotte <loic.descotte@gmail.com>
2012-11-24 09:54:03 +0100

    play 2.1 RC1

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "html5Tags"
-    val appVersion      = "1.0"
+    val appVersion      = "1.0-RC1"
 
     val appDependencies = Seq(
     )
