55d9e781e71434691969e5975abe61be3a77cdcc
Loic Descotte <loic.descotte@gmail.com>
2012-11-25 17:24:13 +0100

    pattern working for java AND scala

--- app/html5/templates/Html5Templates.scala
+++ app/html5/templates/Html5Templates.scala
@@ -2,19 +2,20 @@ package html5.templates
 
 import play.api.templates._
 import java.util._
+import scala.util.matching.Regex
 
 object Html5Templates {
 
  /**
   * Find special types from field
   */
- def findType(field: play.api.data.Field):Html = {
+ def findType(field: play.api.data.Field): Html = {
    val out = Html.empty 
    field.constraints.foreach(constraint => 
-	if(constraint._1.equals("constraint.email"))
-	  return Html("email")
-    )
-    Html("text")
+	 if(constraint._1.equals("constraint.email"))
+	   return Html("email")
+   )
+   Html("text")
   }
   
   /**
@@ -25,19 +26,20 @@ object Html5Templates {
     field.constraints.foreach(constraint => 
       constraint._1 match {
           case "constraint.required" => out + Html(" required")
-          case "constraint.maxLength" => out + constraintValue("maxlength", constraint._2)
-          case "constraint.minLength" => out + Html(" pattern=\".{" + constraint._2.head + ",}\"")
-          case "constraint.pattern" => out + constraintValue("pattern", constraint._2)
+          case "constraint.maxLength" => out + constraintValue("maxlength", constraint._2.headOption)
+          case "constraint.minLength" => out + constraint._2.headOption.map(value => Html(" pattern=\".{" + value + ",}\"")).getOrElse(Html.empty)
+          case "constraint.pattern" => out + (constraint._2.headOption.map(value => value match {
+            //for scala pattern
+            case f: Function0[Any] => constraintValue("pattern", Option(f.apply().toString))
+            //for java @Pattern
+            case value: String => constraintValue("pattern", Option(value))
+          }).getOrElse(Html.empty)) 
           case _ => ()
        })
       out
   }
   
-  private def constraintValue(label: String, values : Seq[Any]) = {
-    val value = values.size match {
-     	  case 0 => None
-      	  case _ => Some(values.head)
-        }  
+  private def constraintValue(label: String, value : Option[Any]) = {
     val out = value.map(" " + label + "=\"" + _ + "\"")
     Html(out.getOrElse(""))
   }
